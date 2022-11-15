89eb84b6033bcf96a2b86dc7ad28265276ad0b75
Jose David <jose7312000@gmail.com>
2022-11-14 19:57:10 -0500

    Trabajando con el envio en tiempo real

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,33 @@ const socket = io('http://localhost:4000')
 function App() {
 
   const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     //enviamos al backend
     socket.emit('message', message)
-    setMessage('')
+
+    const newMessage = {
+      body: message,
+      from: "Me"
+    }
+
+    setMessages([newMessage, ...messages])
+    setMessage("")
   }
 
   useEffect(() => {
     const receiveMessage = (message) => {
-      console.log(message)
+     setMessages([message, ...messages])
     };
-
     socket.on('message', receiveMessage)
 
     return () => {
       socket.off('message', receiveMessage)
     }
-  }, [])
+  }, [messages])
 
   return (
     <div className="App">
@@ -36,6 +43,12 @@ function App() {
        <input type="text" onChange={e => setMessage(e.target.value)} value={message}/>
        <button>send</button>
      </form>
+
+     {messages.map((message, index) => (
+      <div key={index}>
+        <p>{message.from} : {message.body}</p>
+      </div>
+     ))}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -19,8 +19,10 @@ app.use(morgan("dev"))
 io.on('connection', (socket) => {
 
     socket.on('message', (message) => {
-        socket.broadcast.emit('message', message)
-        console.log(message)
+        socket.broadcast.emit('message', {
+            body: message,
+            from: socket.id
+        })
     })
 })
 
